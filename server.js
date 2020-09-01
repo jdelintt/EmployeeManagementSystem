f28f54b292a1e9777d9f1a26eeb1c05d9433d72f
Jesus DeLintt <jesus.delintt@gmail.com>
2020-08-31 18:34:24 -0700

    initialized express

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require("express");
 var mysql = require("mysql");
 const inquirer = require("inquirer");
+var express = require("express");
+
+var app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 require("./app/questions")(app);
 
-
-inquirer.prompt(questions).then((answers) => {
-
-    
-}
+inquirer.prompt(initialQuestion).then((answers) => {});
